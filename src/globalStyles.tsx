75a21408ad17c7ca0bb8a0e6f65ee1ac10979796
felipecarvalho180 <felipecarvalho180@gmail.com>
2021-04-17 19:05:34 -0300

    update font import.

--- src/globalStyles.tsx
+++ src/globalStyles.tsx
@@ -2,7 +2,7 @@ import { createGlobalStyle } from 'styled-components';
 import { BACKGROUND_COLOR } from './style/colors';
 
 const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Work+Sans:wght@100;700&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Work+Sans:wght@300;700&display=swap');
 
   body {
     margin: 0;
